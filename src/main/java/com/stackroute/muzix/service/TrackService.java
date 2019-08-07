b0f3249a83434d3b04e2c35306a77dca2985a38e
Pallavi1610 <51950013+Pallavi1610@users.noreply.github.com>
2019-08-07 12:35:04 +0530

    added Exceptions

--- src/main/java/com/stackroute/muzix/service/TrackService.java
+++ src/main/java/com/stackroute/muzix/service/TrackService.java
@@ -3,19 +3,13 @@ package com.stackroute.muzix.service;
 import com.stackroute.muzix.domain.Track;
 import com.stackroute.muzix.exceptions.TrackAlreadyExistsException;
 import com.stackroute.muzix.exceptions.TrackNotFoundException;
-
 import java.util.List;
 
 public interface TrackService {
-    public Track saveTrack(Track track) throws TrackAlreadyExistsException;
-
-    public void deleteTrack(int id);
-
-    public List<Track> getAllTracks();
-
-    public Track getTrackById(int id) throws TrackNotFoundException;
-
-
-    public Track UpdateTrack(Track track);
-    public List<Track> getTrackByName(String name) throws TrackNotFoundException;
+    Track saveTrack(Track track) throws TrackAlreadyExistsException;
+    void deleteTrack(int id) throws TrackNotFoundException;
+    List<Track> getAllTracks();
+    Track getTrackById(int id) throws TrackNotFoundException;
+    Track updateTrack(Track track) throws TrackNotFoundException;
+    List<Track> getTrackByName(String name) throws TrackNotFoundException;
 }
